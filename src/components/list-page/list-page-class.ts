f9ef0ddcaac5f5e26662aa80229354009fcf1963
Nikita Mayorov <nsmayorov@gmail.com>
2023-12-12 17:04:41 +0300

    change pc

--- src/components/list-page/list-page-class.ts
+++ src/components/list-page/list-page-class.ts
@@ -1,3 +1,15 @@
 export default class List {
+    public arrResult: string[] = []
+    public head: number | null;
+    public tail: number | null;
+    public next: number | null; 
+
+    constructor() {
+        this.arrResult = ['0', '34', '8', '1']
+        this.head = null
+        this.tail = null
+        this.next = null
+    }
+
     
 }
